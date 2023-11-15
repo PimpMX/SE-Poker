9362bd5d9bf3323cf6ed0dde817b8d1083ee9569
lukas <lgkn.personal@gmail.com>
2023-11-15 10:32:01 +0100

    Added ControllerSpec test

--- src/test/scala/de/htwg/controller/ControllerSpec.scala
+++ src/test/scala/de/htwg/controller/ControllerSpec.scala
@@ -6,81 +6,101 @@ import de.htwg.model.GameHandler
 
 class ControllerSpec extends AnyWordSpec with Matchers {
 
+    "Controller" when {
+
+        val gameState = GameHandler.generateGame(2)
+        val controller = new Controller(gameState)
+
+        "created with 2 players" should {
+            
+            "have a valid GameState" in {
+                gameState.isDefined shouldBe(true)
+            }
+
+            "return no game for invalid bet" in {
+                controller.bet(1100) shouldBe(true)
+            }
+
+            "return a game for valid bet" in {
+                controller.bet(100) shouldBe(true)
+            }
+
+            "return a game for valid bet all-in" in {
+                controller.betAllIn() shouldBe(true)
+            }
+
+            "return no game for invalid all-in" in {
+                controller.check()
+                controller.betAllIn() shouldBe(false)
+            }
+
+            "return a game for check" in {
+                controller.check() shouldBe(true)
+            }
+
+            "return a game for fold" in {
+                controller.fold() shouldBe(true)
+            }
+
+            "return no game for invalid fold" in {
+                controller.fold()
+                controller.fold() shouldBe(false)
+            }
+
+            "be able to render the GameState" in {
+                controller.toString() should be(gameState.get.toString())
+            }
+        }
+
+        "created with 11 players" should {
+
+            val gameState = GameHandler.generateGame(11)
+
+            "return no game" in {
+                gameState.isDefined shouldBe(false)
+            }
+        }
+    }
+
     // "Controller" should {
 
     //     val gameState = GameHandler.generateTwoPlayerGame()
+    //     val controller = new Controller(gameState)
 
-    //     "handle new game command correctly" in {
-    //         val newGameState = Controller.userCmd("new game", gameState)
-    //         newGameState.isDefined should be(true)
-    //         newGameState.get should be (gameState)
+    //     "return new game with valid number" in {
+    //         val newGame = controller.newGame(1)
+    //         newGame.shouldBe(true)
+    //     }
+
+    //     "return no game with invalid number" in {
+    //         val newGame = controller.newGame(0)
+    //         newGame.shouldBe(false)
+    //     }
+
+    //     "handle valid bet command correctly" in {
+    //         val bet = controller.bet(100)
+    //         bet.shouldBe(true)
     //     }
 
     //     "handle bet command correctly" in {
-    //         val betCommand = "bet 100"
-    //         val betGameState = Controller.userCmd(betCommand, gameState)
-    //         betGameState.isDefined should be(true)
-    //         betGameState.get.playerAtTurn should be(1)
-    //         betGameState.get should not be (gameState)
+    //         controller.bet(100)
     //     }
 
     //     "handle bet all-in command correctly" in {
-    //         val allInGameState = Controller.userCmd("bet all-in", gameState)
-    //         allInGameState.isDefined should be(true)
-    //         allInGameState.get.playerAtTurn should be(1)
-    //         allInGameState.get should not be (gameState)
+    //         controller.bet_all_in()
     //     }
 
     //     "handle check command correctly" in {
-    //         val checkGameState = Controller.userCmd("check", gameState)
-    //         checkGameState.isDefined should be(true)
-    //         checkGameState.get.playerAtTurn should be(1)
-    //         checkGameState.get should not be (gameState)
+    //         controller.check()
     //     }
 
     //     "handle fold command correctly" in {
-    //         val foldGameState = Controller.userCmd("fold", gameState)
-    //         foldGameState.isDefined should be(true)
-    //         foldGameState.get.playerAtTurn should be(1)
-    //         foldGameState.get should not be (gameState)
+    //         controller.fold()
+    //     }
+
+    //     "return the correct string representation of the game state" in {
+    //         controller.restart_game()
+    //         controller.toString() should be(gameState.toString())
     //     }
     // }
-
-    // "return None for invalid user commands" in {
-    //     val gameState = GameHandler.generateTwoPlayerGame()
-    //     val invalidCommand = "invalid command"
-    //     val invalidGameState = Controller.userCmd(invalidCommand, gameState)
-    //     invalidGameState should be(None)
-    // }
-
-    "Controller" should {
-
-        val gameState = GameHandler.generateTwoPlayerGame()
-        val controller = new Controller(gameState)
-
-        "handle restart game command correctly" in {
-            controller.restart_game()
-        }
-
-        "handle bet command correctly" in {
-            controller.bet(100)
-        }
-
-        "handle bet all-in command correctly" in {
-            controller.bet_all_in()
-        }
-
-        "handle check command correctly" in {
-            controller.check()
-        }
-
-        "handle fold command correctly" in {
-            controller.fold()
-        }
-
-        "return the correct string representation of the game state" in {
-            controller.restart_game()
-            controller.toString() should be(gameState.toString())
-        }
-    }
 }
